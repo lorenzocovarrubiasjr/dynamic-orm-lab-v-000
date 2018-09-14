3d8b598773b9cb57e8f59e7b7d08a51b81ca24c2
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-09-14 19:21:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,6 +58,7 @@ class InteractiveRecord
     
     def self.find_by(options={}) 
       sql = puts  "SELECT * FROM #{self.table_name} WHERE #{options.keys.to_s.delete("[:]")} = #{options.values.to_s.delete("[:]")}"
+      binding.pry 
       DB[:conn].execute(sql)
     end 
 end
