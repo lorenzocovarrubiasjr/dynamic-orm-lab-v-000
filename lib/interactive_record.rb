5011c93b1a6ad55abd1fc0315e597e3676de727e
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-09-14 18:52:31 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
     end 
     
     def self.find_by(options={}) 
-      sql = "SELECT * FROM #{self.table_name} WHERE = "
-      DB[:conn].execute(sql)
+      sql = "SELECT * FROM #{self.table_name} WHERE ? = ?"
+      DB[:conn].execute(sql, )
     end 
 end
